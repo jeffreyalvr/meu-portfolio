8b42020f85f17cbf91f60808a57d4e27d0c4b22d
Jeffrey Ramo <matheusunitt@gmail.com>
2023-03-31 16:30:17 -0300

    Adiciona os elementos section e footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="copyright">
+        feito por <a href="#">@jeffreyalvr</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,19 @@
 import Nav from "../../components/Nav";
 import Hero from "../../components/Hero";
+import Section from "../../components/Section";
+import Footer from "../../components/Footer";
 
 const Home = () => {
   return (
     <div className="wrapper">
       <Nav />
       <Hero />
+      <Section
+        title="Uma section qualquer"
+        description="Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+        variant="light"
+      />
+      <Footer />
     </div>
   );
 };
