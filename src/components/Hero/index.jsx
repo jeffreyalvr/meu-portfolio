72ab4b31d60b88ba64955dad5ad6cbb675064511
Jeffrey <matheusunitt@gmail.com>
2023-08-31 08:50:14 -0300

    Isola o footer como componente próprio

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -10,6 +10,8 @@ import github from "../../assets/images/github.png";
 
 import Button from "../Button";
 
+import Footer from "../Footer";
+
 const Hero = ({ children }) => {
   const linkedin_url = "https://www.linkedin.com/in/jeffreyalvr/";
   const github_url = "https://github.com/jeffreyalvr";
@@ -26,25 +28,19 @@ const Hero = ({ children }) => {
             : book.en_ca.hero.index.hero_h1}
           <span>{"{ Jeffrey }"}</span>
         </h1>
+
         <div className="label-block">
           <h2>react js developer</h2>
           <div className="carrot" />
         </div>
+
         <div className="row">
           <Button text="LinkedIn" icon={linkedin} url={linkedin_url} />
           <Button text="GitHub" icon={github} url={github_url} />
         </div>
+
+        <Footer book={book} lang={lang} />
       </div>
-      <footer className="move-up">
-        <div className="col">
-          <span>
-            {lang === "pt-br"
-              ? book.pt_br.hero.index.hero_footer_span
-              : book.en_ca.hero.index.hero_footer_span}
-          </span>
-          <span>&copy; 2023 - Jeffrey.</span>
-        </div>
-      </footer>
     </div>
   );
 };
