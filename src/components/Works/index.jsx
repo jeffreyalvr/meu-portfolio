2bda5b6b61e6b8b3a2e28ecec315a2972ec556b1
Jeffrey <matheusunitt@gmail.com>
2023-09-24 13:09:01 -0300

    Adiciona botão para abrir projetos; Adiciona tradução para o botão; Adiciona ícone de abrir link externo

--- src/components/Works/index.jsx
+++ src/components/Works/index.jsx
@@ -11,6 +11,7 @@ import { ActionButton } from "../Button";
 
 import thumb from "../../assets/images/projects/thumb.png";
 import view_all_icon from "../../assets/images/view_all.png";
+import open_icon from "../../assets/images/open.png";
 
 const Works = () => {
   const { lang } = useContext(LanguageContext);
@@ -22,36 +23,101 @@ const Works = () => {
     navigate(path);
   };
 
+  const handleViewProject = () => {
+    let path = "/works/1";
+    navigate(path);
+  };
+
   return (
     <div className="works">
       <div className="work-item">
-        <h3>Github Repo Browser</h3>
+        <div className="item-container">
+          <ActionButton
+            overrideClass="invert-img-hover my-20"
+            text={
+              lang === "pt-br"
+                ? book.pt_br.sections.sec_works_project_button
+                : book.en_ca.sections.sec_works_project_button
+            }
+            icon={open_icon}
+            func={handleViewProject}
+          />
+          <h3>Github Repo Browser</h3>
+        </div>
         <img className="banner" src={thumb} />
       </div>
 
       <div className="work-item">
-        <h3>Buscador Imagens</h3>
+        <div className="item-container">
+          <ActionButton
+            overrideClass="invert-img-hover my-20"
+            text={
+              lang === "pt-br"
+                ? book.pt_br.sections.sec_works_project_button
+                : book.en_ca.sections.sec_works_project_button
+            }
+            icon={open_icon}
+            func={handleViewProject}
+          />
+          <h3>Buscador Imagens</h3>
+        </div>
         <img className="banner" src={thumb} />
       </div>
 
       <div className="work-item">
-        <h3>Weather App</h3>
+        <div className="item-container">
+          <ActionButton
+            overrideClass="invert-img-hover my-20"
+            text={
+              lang === "pt-br"
+                ? book.pt_br.sections.sec_works_project_button
+                : book.en_ca.sections.sec_works_project_button
+            }
+            icon={open_icon}
+            func={handleViewProject}
+          />
+          <h3>Weather App</h3>
+        </div>
         <img className="banner" src={thumb} />
       </div>
 
       <div className="work-item">
-        <h3>Todo List Simples</h3>
+        <div className="item-container">
+          <ActionButton
+            overrideClass="invert-img-hover my-20"
+            text={
+              lang === "pt-br"
+                ? book.pt_br.sections.sec_works_project_button
+                : book.en_ca.sections.sec_works_project_button
+            }
+            icon={open_icon}
+            func={handleViewProject}
+          />
+          <h3>Todo List Simples</h3>
+        </div>
         <img className="banner" src={thumb} />
       </div>
 
       <div className="work-item">
-        <h3>Minha Lista de Compras</h3>
+        <div className="item-container">
+          <ActionButton
+            overrideClass="invert-img-hover my-20"
+            text={
+              lang === "pt-br"
+                ? book.pt_br.sections.sec_works_project_button
+                : book.en_ca.sections.sec_works_project_button
+            }
+            icon={open_icon}
+            func={handleViewProject}
+          />
+          <h3>Minha Lista de Compras</h3>
+        </div>
         <img className="banner" src={thumb} />
       </div>
 
       <div className="work-item">
         <ActionButton
-          overrideClass="invert-img-hover"
+          overrideClass="invert-img-hover my-20"
           text={
             lang === "pt-br"
               ? book.pt_br.sections.sec_works_all_projects_button
